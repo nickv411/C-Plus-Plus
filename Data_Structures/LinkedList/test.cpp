7ee3f7030e780036af509ef5113d78652f60cc1d
nickv411 <vincent.nicholas.d@gmail.com>
2026-04-17 21:55:52 -0400

    Framing out more testing

--- Data_Structures/LinkedList/test.cpp
+++ Data_Structures/LinkedList/test.cpp
@@ -9,6 +9,9 @@
 using namespace std;
 
 
+/**
+*         GENERIC TESTS START
+*/
 
 /**
 * Test creating new list
@@ -74,6 +77,87 @@ EXIT:
 }
 
 /**
+* Test add/remove
+*/
+bool test_add_remove(){
+    LinkedList * p_list = new LinkedList();
+    bool ret_val = true;
+    int one = 1;
+    int two = 2;
+    int three = 3;
+
+    if (0 != p_list->len())
+    {
+        ret_val = false;
+        goto EXIT;
+    }
+
+    p_list->insert_front(&one);
+    p_list->insert_front(&two);
+    p_list->insert_front(&three);
+
+    if (3 != p_list->len())
+    {
+        ret_val = false;
+        goto EXIT;
+    }
+
+    ret_val = p_list->remove_front();
+
+    if (2 != p_list->len())
+    {
+        ret_val = false;
+        goto EXIT;
+    }
+
+    ret_val = p_list->remove_back();
+
+    if (1 != p_list->len())
+    {
+        ret_val = false;
+        goto EXIT;
+    }
+
+EXIT:
+    return ret_val;
+}
+
+/**
+*         GENERIC TESTS END
+*/
+
+/**
+*         ADD TESTS START
+*/
+
+// Add to null linkedlist?
+
+// Add NULL ptr to linkedlist
+
+// Add to linked list with one value
+
+// Add to linked list with multiple values
+
+/**
+*         ADD TESTS END
+*/
+
+/**
+*         REMOVE TESTS START
+*/
+
+// Remove from NULL linkedList?
+
+// Remove from empty linked list
+
+// Remove head from linked list
+// Remove again?
+
+// Remove front
+
+// Remove back
+
+/**
 * Main function to run tests
 */
 int main (int argc, char ** argv){
@@ -82,4 +166,5 @@ int main (int argc, char ** argv){
     cout << "Test constructor: " << test_new_list() << endl;
     cout << "Test destructor: " << test_delete_list() << endl;
     cout << "Test len: " << test_len_list() << endl;
+    cout << "Test add/remove: " << test_add_remove() << endl;
 }
